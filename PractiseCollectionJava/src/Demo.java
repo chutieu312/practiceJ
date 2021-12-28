0f56cf1743a0e5fca9b6661c1f8e1457c589c515
CanD Nguyen <vodanhchutieu@gmail.com>
2021-12-27 22:38:03 -0500

    hello

---
+++ PractiseCollectionJava/src/Demo.java
@@ -0,0 +1,35 @@
+
+
+
+
+public class Demo {
+		
+	public static void main(String[] args) {
+		
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+	
+		System.out.println("hello world");
+
+		
+		for(;;) {
+			System.out.println("hehehehe");
+			System.out.println("hehehehe");
+			break;
+		}
+	
+	}
+	
+	
+	// tao ne
+
+}
